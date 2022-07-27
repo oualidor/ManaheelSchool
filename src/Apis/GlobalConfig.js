bb675bbddd81d8ba50694022cdfbbae23bf2389e
Oualid KHIAL <walid.khial@gmail.com>
2022-07-27 15:50:03 +0100

    Change server location

--- src/Apis/GlobalConfig.js
+++ src/Apis/GlobalConfig.js
@@ -1,4 +1,4 @@
-// const  SERVER = "https://fierce-thicket-72057.herokuapp.com/admin/";
-const  SERVER = "http://localhost:5000/admin/";
+const  SERVER = "https://fierce-thicket-72057.herokuapp.com/admin/";
+// const  SERVER = "http://localhost:5000/admin/";
 
 module.exports = SERVER;
